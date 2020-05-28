389fd0a18aad644e6d98ad78c5c122c66c982cc1
Lacote <mat.lac702@gmail.com>
2020-05-28 08:23:22 +0200

    added test programs

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ cd ../interpreter
 make
 cd ../
 ./compiler/compiler programs/my_program.c programs/my_program.asm
-./interpreter/interpreter programs/my_program.asm 
+./interpreter/interpreter programs/my_program.asm
